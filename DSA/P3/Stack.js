1d28ccab996b70ab25ba0a263ec68146c7fd3b07
ashishguptabns <ashish.gupta@albertinvent.com>
2024-01-07 23:20:47 +0530

    P3

--- DSA/P3/Stack.js
+++ DSA/P3/Stack.js
@@ -1,6 +1,19 @@
 /* Valid parentheses - Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
  */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 const isValid = (s) => {
+
+    /* pseudo code
+        move through chars of s
+            found a closing bracket
+                check if top of stack is opposite bracket
+            found an opening bracket
+                push to the stack
+    */
+
     const stack = []
     const bracketMap = {
         ')': '(',
@@ -8,9 +21,8 @@ const isValid = (s) => {
         ']': '['
     };
 
-    for (let i = 0; i < s.length; i++) {
-        const currentBracket = s[i]
-        if (bracketMap.hasOwnProperty(currentBracket)) {
+    for (const currentBracket of s) {
+        if (bracketMap[currentBracket]) {
             // If it's a closing bracket
             const topElement = stack.length === 0 ? '#' : stack.pop();
             if (bracketMap[currentBracket] !== topElement) {
@@ -25,51 +37,6 @@ const isValid = (s) => {
     return stack.length === 0
 };
 
-/* Decode String - Given an encoded string, return its decoded string.
-
-The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
- */
-/**
- * @param {string} s
- * @return {string}
- */
-const decodeString = (s) => {
-    const stack = []
-    for (const char of s) {
-        if (char !== ']') {
-            //  keep pushing till the closing brakcet
-            stack.push(char)
-            continue
-        }
-        //  found a closing bracket
-
-        let stackTop = stack.pop()
-        let currStr = ''
-
-        //  find string till the last open bracket
-        while (stackTop !== '[') {
-            currStr = stackTop + currStr
-            stackTop = stack.pop()
-        }
-
-        //  find the number
-        let num = ''
-        stackTop = stack.pop()
-        while (!Number.isNaN(Number(stackTop))) {
-            num = stackTop + num
-            stackTop = stack.pop()
-        }
-
-        //  notice we popped an extra element
-        stack.push(stackTop)
-
-        //  push for futher travel
-        stack.push(currStr.repeat(Number(num)))
-    }
-
-    return stack.join('')
-};
-
 /* Remove All Adjacent Duplicates in String  - You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
 
 We repeatedly make k duplicate removals on s until we no longer can.
@@ -82,6 +49,19 @@ Return the final string after all such duplicate removals have been made. It is 
  * @return {string}
  */
 const removeDuplicates = (s, k) => {
+
+    /* pseudo code
+        keep a stack
+            each element is [char, freq]
+        move though chars of s
+            check if found a duplicate
+                increase the freq
+                freq matches k
+                    remove the top of stack
+            else keep pushing to the stack
+        go through the stack and recreate the string
+    */
+
     if (s.length < k) {
         return s
     }
@@ -112,109 +92,29 @@ const removeDuplicates = (s, k) => {
     return res
 };
 
-/* Daily Temperatures - Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
- */
-/**
- * @param {number[]} temperatures
- * @return {number[]}
- */
-const dailyTemperatures = (temps) => {
-    const stack = []
-    const answer = Array(temps.length).fill(0)
-
-    for (let i = 0; i < temps.length; i++) {
-        while (stack.length && temps[stack[stack.length - 1]] < temps[i]) {
-            //  found a warmer temp
-
-            //  pop all the cold temp indexes and record the days
-            const coldIndex = stack.pop()
-            answer[coldIndex] = i - coldIndex
-        }
-
-        //  add this index as cold
-        stack.push(i)
-    }
-
-    return answer
-};
-
-/* Online Stock Span - Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
-
-The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
- */
-class StockSpanner {
-    //  this stack will keep a price and its span
-    stack = []
-    next(price) {
-        const { stack } = this
-
-        //  includes the current day by default
-        let days = 1
-
-        //  find days when stock price was less than today to find total span for this price
-        //  notice that there can be multiple spansw
-        while (stack.length && stack.at(-1).price <= price) {
-            //  count prev lower days also
-            days += stack.pop().days
-        }
-
-        //  push the current one for future days
-        stack.push({ price, days })
-        return days
-    }
-}
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * var obj = StockSpanner()
- * var param_1 = obj.next(price)
- */
-
-
-
-/* Next Greater Element - Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
-
-The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
- */
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-const nextGreaterElements = (nums) => {
-    const res = []
-    const stack = []
-
-    //  travel twice to avoid circular effect
-    for (let i = 2 * nums.length - 1; i >= 0; i--) {
-        //  find the index which has greater item than current item
-        while (stack.length && nums[stack.at(-1)] <= nums[i % nums.length]) {
-            stack.pop()
-        }
-
-        if (stack.length === 0) {
-            //  even after 2 rounds no greater item found
-            res[i % nums.length] = -1
-        } else {
-            //  top element of stack is the next greater item for current item
-            res[i % nums.length] = nums[stack.at(-1)]
-        }
-
-        stack.push(i % nums.length)
-    }
-
-    return res
-};
-
 /* Minimum add to make parentheses valid - find the minimum number of parentheses to add to make a given string of parentheses valid
  */
 const minAddToMakeValid = (s) => {
+
+    /* pseudo code
+        move through chars of s
+            opening bracket
+                move to stack
+            closing bracket
+                top of stack is opening bracket
+                    pop from stack
+                else
+                    push to stack
+        return length of stack
+    */
+
     const stack = []
 
     for (const char of s) {
         if (char === '(') {
             stack.push(char)
         } else {
-            if (stack.length > 0 && stack[stack.length - 1] === '(') {
+            if (stack.length > 0 && stack.at(-1) === '(') {
                 //  found a match
                 stack.pop()
             } else {
@@ -244,6 +144,22 @@ It can be written as (A), where A is a valid string.
  * @return {string}
  */
 const minRemoveToMakeValid = (s) => {
+
+    /* pseudo code
+        split the s into char array to make in place changes
+        keep a stack to track indices of invalid brackets
+        move through chars of s
+            opening bracket
+                push to stack
+            closing bracket
+                opening brackets in stack
+                    pop from stack
+                make current item blank to remove invalid bracket
+        move through stack
+            find the indices and make them blank
+        join the s and return
+    */
+
     s = s.split('')
 
     const stack = []
@@ -270,6 +186,175 @@ const minRemoveToMakeValid = (s) => {
     return s.join('')
 };
 
+/* Decode String - Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const decodeString = (s) => {
+
+    /* pseudo code
+        move thorugh chars of s
+            not a closing bracket
+                push to stack
+            closing bracket
+                pop from stack till opening bracket and make the string
+                pop from stack till the top element is a num
+            decode the string and push back to the stack for further travel
+        join all elements of the stack
+    */
+
+    const stack = []
+    for (const char of s) {
+        if (char !== ']') {
+            //  keep pushing till the closing brakcet
+            stack.push(char)
+            continue
+        }
+        //  found a closing bracket
+
+        let stackTop = stack.pop()
+        let currStr = ''
+
+        //  find string till the last open bracket
+        while (stackTop !== '[') {
+            currStr = stackTop + currStr
+            stackTop = stack.pop()
+        }
+        //  remove the opening bracket
+        stackTop = stack.pop()
+
+        //  find the number
+        let num = ''
+        while (!Number.isNaN(Number(stackTop))) {
+            num = stackTop + num
+            stackTop = stack.pop()
+        }
+
+        //  notice we popped an extra element
+        stack.push(stackTop)
+
+        //  push for futher travel
+        stack.push(currStr.repeat(Number(num)))
+    }
+
+    return stack.join('')
+};
+
+/* Daily Temperatures - Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+ */
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+const dailyTemperatures = (temps) => {
+
+    /* pseudo code
+        keep a stack to store indices of days for which we have to find warmer days
+        move i through temps array
+            loop through stack till we find a warmer temp than ith temp
+                pop the cold index
+                store num days for this cold index
+            keep pushing i in the stack
+    */
+
+    const stack = []
+    const answer = Array(temps.length).fill(0)
+
+    for (let i = 0; i < temps.length; i++) {
+        while (stack.length && temps[stack.at(-1)] < temps[i]) {
+            //  found a warmer temp
+
+            //  pop all the cold temp indexes and record the days
+            const coldIndex = stack.pop()
+            answer[coldIndex] = i - coldIndex
+        }
+
+        //  add this index as cold
+        stack.push(i)
+    }
+
+    return answer
+};
+
+/* Online Stock Span - Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+ */
+class StockSpanner {
+
+    /* pseudo code
+        keep a stack
+            item is [price, span]
+        loop through the stack till we find a greater price than curr price
+            pop the item
+            track num days
+        push the price and span to the stack
+        return span
+    */
+
+    //  this stack will keep a price and its span
+    stack = []
+    next = (price) => {
+        const { stack } = this
+
+        //  includes the current day by default
+        let days = 1
+
+        //  find days when stock price was less than today to find total span for this price
+        //  notice that there can be multiple spans
+        while (stack.length && stack.at(-1).price <= price) {
+            //  count prev lower days also
+            days += stack.pop().days
+        }
+
+        //  push the current one for future days
+        stack.push({ price, days })
+        return days
+    }
+}
+
+/* Next Greater Element - Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const nextGreaterElements = (nums) => {
+
+    /* pseudo code
+            
+    */
+
+    const res = []
+    const stack = []
+
+    //  travel twice to avoid circular effect
+    for (let i = 2 * nums.length - 1; i >= 0; i--) {
+        //  find the index which has greater item than current item
+        while (stack.length && nums[stack.at(-1)] <= nums[i % nums.length]) {
+            stack.pop()
+        }
+
+        if (stack.length === 0) {
+            //  even after 2 rounds no greater item found
+            res[i % nums.length] = -1
+        } else {
+            //  top element of stack is the next greater item for current item
+            res[i % nums.length] = nums[stack.at(-1)]
+        }
+
+        stack.push(i % nums.length)
+    }
+
+    return res
+};
+
 /* Asteroid Collision - We are given an array asteroids of integers representing asteroids in a row.
 
 For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
@@ -282,10 +367,22 @@ Find out the state of the asteroids after all collisions. If two asteroids meet,
  * @return {number[]}
  */
 const asteroidCollision = (asteroids) => {
-    // Initialize an empty stack to keep track of asteroids
+
+    /* pseudo code
+        move through given asteroids
+            negative speed
+                loop through stack to find lighter asteroids moving right
+                    keep popping
+                found an equal weight asteroid moving to right
+                    kill it and pop from stack
+                found a heavier asteroid in the stack
+                    ignore
+            keep pushing to stack as current asteroid survives
+        return left asteroids in the stack
+    */
+
     const stack = []
 
-    // Iterate through each asteroid in the input array
     for (const a of asteroids) {
         // Check if the current asteroid is moving to the left (negative value)
         if (a < 0) {
@@ -296,8 +393,9 @@ const asteroidCollision = (asteroids) => {
             }
 
             // Check if the current asteroid annihilates the asteroid at the top of the stack
-            if (stack.length > 0 && stack.at(-1) + a === 0 && stack.pop()) {
+            if (stack.length > 0 && stack.at(-1) + a === 0) {
                 // If the top asteroid is completely destroyed, continue to the next iteration
+                stack.pop()
                 continue
             }
 
@@ -316,7 +414,7 @@ const asteroidCollision = (asteroids) => {
     return stack
 };
 
-/* Sum of Subarray Minimums - Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7.
+/* Sum of Subarray Minimums - Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 10**9 + 7.
  */
 /**
  * @param {number[]} arr
@@ -369,3 +467,4 @@ const sumSubarrayMins = (arr) => {
     // Return the final result
     return result;
 }
+
