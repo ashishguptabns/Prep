2e17146a5864aef48f1f5d881713e43c5ea62950
ashishguptabns <ashish.gupta@albertinvent.com>
2024-01-08 23:02:13 +0530

    P1

--- DSA/P1/Graph.js
+++ DSA/P1/Graph.js
@@ -146,8 +146,8 @@ const minSemesters = (n, prerequisites, k) => {
       prereq -> array of courses
       maintain indegree of each course - how many prereqs are needed
     start BFS with courses which have 0 prereqs
-      decide number of courses to process
-      go through kids and reduce the indegree cause this course has been taken
+      decide number of courses to process in one sem
+      go through next courses and reduce the indegree cause this course has been taken
       if a kid has 0 indegree 
         add to the queue as it is ready to be taken
       keep tracking num sems
@@ -200,8 +200,8 @@ const minSemesters = (n, prerequisites, k) => {
     semesters++;
   }
 
-  for (let i = 1; i <= n; i++) {
-    if (indegree[i] > 0) {
+  for (const deg of indegree) {
+    if (deg > 0) {
       // If there are still courses with prerequisites, it's impossible to complete all courses
       return -1;
     }
@@ -241,11 +241,7 @@ const getAncestors = (n, edges) => {
     graph[from].push(to);
   }
 
-  // Initialize output array
-  const ancestors = Array(n);
-  for (let i = 0; i < ancestors.length; i++) {
-    ancestors[i] = [];
-  }
+  const ancestors = new Array(n).fill().map(() => []);
 
   // Process all ancestors from 0 to n. This will ensure sorted order in the output.
   for (let originNode = 0; originNode < n; originNode++) {
@@ -358,131 +354,6 @@ const findOrder = (numCourses, prerequisites) => {
   return numCourses === 0 ? res : [];
 };
 
-/* Find eventual safe states - There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0 - indexed 2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from node i to each node in graph[i].
-
-A node is a terminal node if there are no outgoing edges.A node is a safe node if every possible path starting from that node leads to a terminal node(or another safe node).
-
-Return an array containing all the safe nodes of the graph.The answer should be sorted in ascending order.
- */
-/**
- * @param {number[][]} graph
- * @return {number[]}
- */
-const eventualSafeNodes = (graph) => {
-
-  /* pseudo code
-    go through each node in the graph
-      run a DFS to check if it is a safe node
-        keep a map to maintain safe/unsafe boolean for each node
-        run a DFS for each neighbour
-        set true in map if no unsafe nodes are found
-  */
-
-  const ans = [];
-  const map = new Map();
-  const dfs = (graph, node, map) => {
-    if (map.has(node)) {
-      return map.get(node);
-    }
-    //  this will help find circular or unsafe nodes
-    map.set(node, false);
-
-    //  check all the neighbors
-    for (let nei of graph[node]) {
-      if (!dfs(graph, nei, map)) {
-        //  this neighbor is not safe
-        return false;
-      }
-    }
-
-    //  all the neighbors are safe
-    map.set(node, true);
-    return true;
-  }
-  for (let i = 0; i < graph.length; i++) {
-    if (dfs(graph, i, map)) {
-      ans.push(i);
-    }
-  }
-  return ans;
-};
-
-/* Find Closest Node to Given Two Nodes - You are given a directed graph of n nodes numbered from 0 to n - 1, where each node has at most one outgoing edge.
-
-The graph is represented with a given 0 - indexed array edges of size n, indicating that there is a directed edge from node i to node edges[i].If there is no outgoing edge from i, then edges[i] == -1.
-
-You are also given two integers node1 and node2.
-
-Return the index of the node that can be reached from both node1 and node2, such that the maximum between the distance from node1 to that node, and from node2 to that node is minimized.If there are multiple answers, return the node with the smallest index, and if no possible answer exists, return -1.
-
-Note that edges may contain cycles.
- */
-/**
- * @param {number[]} edges
- * @param {number} node1
- * @param {number} node2
- * @return {number}
- */
-const closestMeetingNode = (edges, node1, node2) => {
-
-  /* pseudo code
-    keep two maps and store distances of each node from node1 and node2
-      notice circular edges
-    go through the edges
-      for each node find the max distance between node1 and node2
-      if distance is less than max so far
-        update the distance and the node
-  */
-
-  // Initialize two maps to store the distances from node1 and node2 to each node in the graph.
-  const map1 = {};
-  const map2 = {};
-
-  // Initialize a counter to keep track of the distance from the starting node (node1 or node2).
-  let count = 0;
-
-  // Populate map1 with distances from node1 to each node in the graph.
-  while (map1[node1] == undefined && node1 != -1) {
-    map1[node1] = count;
-    count++;
-    node1 = edges[node1];
-  }
-
-  // Reset the counter for map2.
-  count = 0;
-
-  // Populate map2 with distances from node2 to each node in the graph.
-  while (map2[node2] == undefined && node2 != -1) {
-    map2[node2] = count;
-    count++;
-    node2 = edges[node2];
-  }
-
-  // Initialize variables to find the maximum of the minimum distances between node1 and node2.
-  let max = Infinity;
-  let res = -1;
-
-  // Iterate through each node in the graph.
-  for (let i = 0; i < edges.length; i++) {
-    // If either map1 or map2 doesn't have information about the distance to the current node, skip to the next iteration.
-    if (map1[i] == undefined || map2[i] == undefined) {
-      continue;
-    }
-
-    // Calculate the maximum of the distances from node1 and node2 to the current node.
-    let localMax = Math.max(map1[i], map2[i]);
-
-    // Update the result if the current node provides a smaller maximum distance.
-    if (localMax < max) {
-      max = localMax;
-      res = i;
-    }
-  }
-
-  // Return the node that minimizes the maximum distance from both node1 and node2.
-  return res;
-};
-
 /* Alien Dictionary - There is a new alien language that uses the English alphabet. However, the order among the letters is unknown to you.
 
 You are given a list of strings words from the alien language's dictionary, where the strings in words are sorted lexicographically by the rules of this new language.
@@ -505,12 +376,9 @@ const alienOrder = (words) => {
       move the char with 0 indegree to the queue
   */
 
-  // Create a map to represent the graph of characters and their relationships
   const charGraph = new Map();
-  // Create a map to store the indegree (number of incoming edges) for each character
   const inDegree = new Map();
 
-  // Initialize the graph nodes and indegree for each node
   for (const word of words) {
     for (const char of word) {
       inDegree.set(char, 0);
@@ -518,28 +386,21 @@ const alienOrder = (words) => {
     }
   }
 
-  // Iterate through each pair of adjacent words
   for (let i = 0; i < words.length - 1; i++) {
     const currWord = words[i];
     const nextWord = words[i + 1];
 
-    // Check lexicographical order
     if (currWord.startsWith(nextWord) && currWord.length > nextWord.length) {
-      // If the current word starts with the next word and is longer, it's not valid
       return "";
     }
 
-    // Traverse through each character in the smaller word
     for (let j = 0; j < Math.min(currWord.length, nextWord.length); j++) {
       const currWordChar = currWord[j];
       const nextWordChar = nextWord[j];
 
-      // If characters are different, create an edge in the graph
       if (currWordChar !== nextWordChar) {
         if (!charGraph.get(currWordChar).has(nextWordChar)) {
-          // Build the edge
           charGraph.get(currWordChar).add(nextWordChar);
-          // Increase the indegree of the next node
           inDegree.set(nextWordChar, (inDegree.get(nextWordChar) || 0) + 1);
         }
         // Take only one new edge per word
@@ -548,41 +409,29 @@ const alienOrder = (words) => {
     }
   }
 
-  // Create a queue for Breadth-First Search (BFS)
   const bfsQueue = [];
-  // Array to store the result (topological order)
   const result = [];
-  // Set to keep track of visited nodes
   const seen = new Set();
 
-  //  Initialize the BFS queue with nodes having an indegree of 0
-  //  potential starting chars of the dictionary
   for (const [char, degree] of inDegree) {
     if (degree === 0) {
       bfsQueue.push(char);
     }
   }
 
-  // Perform BFS
   while (bfsQueue.length > 0) {
-    // Dequeue a character from the queue
     const currChar = bfsQueue.shift();
-    // Append the character to the result
     result.push(currChar);
-    // Mark the character as visited
     seen.add(currChar);
 
-    // Update indegrees of adjacent characters and enqueue those with indegree 0
     for (const nextChar of charGraph.get(currChar)) {
       inDegree.set(nextChar, inDegree.get(nextChar) - 1);
       if (inDegree.get(nextChar) === 0) {
-        //  unlocked a new char
         bfsQueue.push(nextChar);
       }
     }
   }
 
-  // Check if all nodes have been visited (no cycles) and return the result as a string
   return seen.size === charGraph.size ? result.join('') : '';
 }
 
@@ -836,5 +685,128 @@ const solve = (board) => {
       }
     }
   }
+};
+
+/* Find eventual safe states - There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0 - indexed 2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from node i to each node in graph[i].
+
+A node is a terminal node if there are no outgoing edges.A node is a safe node if every possible path starting from that node leads to a terminal node(or another safe node).
+
+Return an array containing all the safe nodes of the graph.The answer should be sorted in ascending order.
+ */
+/**
+ * @param {number[][]} graph
+ * @return {number[]}
+ */
+const eventualSafeNodes = (graph) => {
+
+  /* pseudo code
+    go through each node in the graph
+      run a DFS to check if it is a safe node
+        keep a map to maintain safe/unsafe boolean for each node
+        run a DFS for each neighbour
+        set true in map if no unsafe nodes are found
+  */
+
+  const ans = [];
+  const map = new Map();
+  const dfs = (graph, node, map) => {
+    if (map.has(node)) {
+      return map.get(node);
+    }
+    //  this will help find circular or unsafe nodes
+    map.set(node, false);
+
+    //  check all the neighbors
+    for (let nei of graph[node]) {
+      if (!dfs(graph, nei, map)) {
+        //  this neighbor is not safe
+        return false;
+      }
+    }
+
+    //  all the neighbors are safe
+    map.set(node, true);
+    return true;
+  }
+  for (let i = 0; i < graph.length; i++) {
+    if (dfs(graph, i, map)) {
+      ans.push(i);
+    }
+  }
+  return ans;
+};
+
+/* Find Closest Node to Given Two Nodes - You are given a directed graph of n nodes numbered from 0 to n - 1, where each node has at most one outgoing edge.
+
+The graph is represented with a given 0 - indexed array edges of size n, indicating that there is a directed edge from node i to node edges[i].If there is no outgoing edge from i, then edges[i] == -1.
+
+You are also given two integers node1 and node2.
+
+Return the index of the node that can be reached from both node1 and node2, such that the maximum between the distance from node1 to that node, and from node2 to that node is minimized.If there are multiple answers, return the node with the smallest index, and if no possible answer exists, return -1.
+
+Note that edges may contain cycles.
+ */
+/**
+ * @param {number[]} edges
+ * @param {number} node1
+ * @param {number} node2
+ * @return {number}
+ */
+const closestMeetingNode = (edges, node1, node2) => {
+
+  /* pseudo code
+    keep two maps and store distances of each node from node1 and node2
+      notice circular edges
+    go through the edges
+      for each node find the max distance between node1 and node2
+      if distance is less than max so far
+        update the distance and the node
+  */
+
+  const map1 = {};
+  const map2 = {};
+
+  let distance = 0;
+
+  // Populate map1 with distances from node1 to each node in the graph.
+  while (!map1[node1] && node1 !== -1) {
+    map1[node1] = distance;
+    distance++;
+    node1 = edges[node1];
+  }
+
+  // Reset the counter for map2.
+  distance = 0;
+
+  // Populate map2 with distances from node2 to each node in the graph.
+  while (!map2[node2] && node2 !== -1) {
+    map2[node2] = distance;
+    distance++;
+    node2 = edges[node2];
+  }
+
+  // Initialize variables to find the maximum of the minimum distances between node1 and node2.
+  let max = Infinity;
+  let res = -1;
+
+  // Iterate through each node in the graph.
+  for (let i = 0; i < edges.length; i++) {
+    // If either map1 or map2 doesn't have information about the distance to the current node, skip to the next iteration.
+    if (!map1[i] || !map2[i]) {
+      continue;
+    }
+
+    // Calculate the maximum of the distances from node1 and node2 to the current node.
+    let localMax = Math.max(map1[i], map2[i]);
+
+    // Update the result if the current node provides a smaller maximum distance.
+    if (localMax < max) {
+      max = localMax;
+      res = i;
+    }
+  }
+
+  // Return the node that minimizes the maximum distance from both node1 and node2.
+  return res;
 };
 
