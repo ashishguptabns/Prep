73c261236a64588658cabcc1e3a98a04f82cb613
ashishguptabns <ashish.gupta@albertinvent.com>
2024-01-14 12:10:33 +0530

    P3

--- DSA/P3/Array.js
+++ DSA/P3/Array.js
@@ -8,11 +8,11 @@ If there is no common prefix, return an empty string ""
 const longestCommonPrefix = (strs) => {
   let lcp = ''
   for (let i = 0; i < strs[0].length; i++) {
-      if (strs.every(str => str[i] === strs[0][i])) {
-          lcp += strs[0][i]
-      } else {
-          break
-      }
+    if (strs.every(str => str[i] === strs[0][i])) {
+      lcp += strs[0][i]
+    } else {
+      break
+    }
   }
 
   return lcp
@@ -51,16 +51,12 @@ const productExceptSelf = (nums) => {
   let rightProduct = 1;
 
   for (let i = 0; i < size; i++) {
-    //  put the product of elements on the left
     res[i] *= leftProduct;
-    //  update the product
     leftProduct *= nums[i];
   }
 
   for (let i = size - 1; i >= 0; i--) {
-    //  put the product of elements on the right
     res[i] *= rightProduct;
-    //  update the product
     rightProduct *= nums[i];
   }
 
@@ -90,11 +86,9 @@ const subArrayRanges = (nums) => {
 
   let sum = 0;
   for (let start = 0; start < nums.length; start++) {
-    //  min max for subarrays starting with i
     let min = nums[start];
     let max = nums[start];
     for (let end = start + 1; end < nums.length; end++) {
-      //  min max for subarrays ending with j
       min = Math.min(min, nums[end]);
       max = Math.max(max, nums[end]);
 
@@ -103,82 +97,6 @@ const subArrayRanges = (nums) => {
   }
 
   return sum;
-};
-
-/* Maximum Subarray - Given an integer array nums, find the subarray with the largest sum, and return its sum.
- */
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const maxSubArray = (nums) => {
-
-  /* pseudo code
-    move through nums 
-      keep tracking the sum of curr sub array and max found so far
-      ditch the subarray when the sum becomes negative
-  */
-
-  let max = -Infinity;
-  let currSubSum = 0;
-
-  for (const num of nums) {
-    currSubSum += num;
-    max = Math.max(max, currSubSum);
-    // If the current subarray sum becomes negative, reset it to 0, as we want to find the maximum subarray sum
-    currSubSum = Math.max(currSubSum, 0);
-  }
-
-  return max;
-};
-
-/* Longest consecutive sequence - Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
-
-You must write an algorithm that runs in O(n) time.
- */
-
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const longestConsecutive = (nums) => {
-
-  /* pseudo code
-    move through nums
-      check if curr num is the end of a sequence
-        loop through the set with decreasing num
-          track the count
-      track the max length of increasing seq
-  */
-
-  // Create a set to efficiently check the presence of elements.
-  const set = new Set(nums);
-
-  // Initialize a variable to store the maximum length.
-  let max = 0;
-
-  // Iterate through each element in the array.
-  for (const num of nums) {
-    // If the next consecutive element is present in the set, skip to the next iteration.
-    if (set.has(num + 1)) {
-      continue;
-    }
-
-    // If the next consecutive element is not present, start counting the consecutive elements backward.
-    let counter = 1;
-    let curr = num;
-
-    // Continue counting consecutive elements backward.
-    while (set.has(--curr)) {
-      counter++;
-    }
-
-    // Update the maximum length.
-    max = Math.max(max, counter);
-  }
-
-  // Return the maximum length of consecutive subsequence.
-  return max;
 };
 
 /* Flip String to Monotone Increasing - A binary string is monotone increasing if it consists of some number of 0's (possibly none), followed by some number of 1's (also possibly none).
@@ -198,7 +116,7 @@ const minFlipsMonoIncr = (s) => {
       found a 1
         track number of 1s
       found a 0
-        either flip all 1s or flip curr char
+        either flip all 1s or flip curr 0
   */
 
   let numOfOnesSoFar = 0
@@ -206,10 +124,8 @@ const minFlipsMonoIncr = (s) => {
 
   for (const c of s) {
     if (c === '1') {
-      //  this is monotone increasing, no flips required             
       numOfOnesSoFar++
     } else {
-      //  we might have to flip this character '0' or all the 1s we found so far                
       numFlips = Math.min(numOfOnesSoFar, numFlips + 1)
     }
   }
@@ -230,118 +146,30 @@ Return the minimum time Bob needs to make the rope colorful.
  * @return {number}
  */
 const minCostRope = (colors, neededTime) => {
+
+  /* pseudo code
+    move i through colors
+      same color to prev color
+        curr color needs more time
+          keep the prev color
+        else
+          keep curr color
+      keep tracking time needed
+  */
+
   let time = 0;
 
-  //  start from 2nd balloon
   for (let i = 1; i < colors.length; i++) {
-    //  found the consecutive same color balloons
     if (colors[i] === colors[i - 1]) {
-      // If the needed time for the current balloon is greater than the needed time for the previous balloon
       if (neededTime[i] > neededTime[i - 1]) {
-        // Add the needed time of the previous balloon to the total time
         time += neededTime[i - 1];
       } else {
-        // Add the needed time of the current balloon to the total time
         time += neededTime[i];
       }
     }
   }
 
   return time;
-};
-
-/* Maximum Product Subarray - Given an integer array nums, find a subarray that has the largest product, and return the product
- */
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const maxProductS = (nums) => {
-
-  /* pseudo code
-    track max and min products found so far
-    move i through the nums array
-      check if curr num increase max or min product or we should discard the prev subarray
-      keep tracking the max
-  */
-
-  if (!nums.length) {
-    return 0;
-  }
-
-  let maxProductSoFar = nums[0];
-  let minProductSoFar = nums[0];
-
-  let max = maxProductSoFar;
-
-  for (let i = 1; i < nums.length; i++) {
-    const curr = nums[i];
-
-    //  either we discard the prev subarray or we choose
-    const tempMax = Math.max(
-      curr,
-      Math.max(curr * maxProductSoFar, curr * minProductSoFar)
-    );
-
-    //  because we have negative numbers; minProductSoFar can make big number later
-    minProductSoFar = Math.min(
-      curr,
-      Math.min(curr * maxProductSoFar, curr * minProductSoFar)
-    );
-
-    maxProductSoFar = tempMax;
-
-    max = Math.max(max, maxProductSoFar);
-  }
-
-  return max;
-};
-
-/* Max sum circular subarray - Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
-
-A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
-
-A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
- */
-
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const maxSubarraySumCircular = (nums) => {
-  // Initialize variables to keep track of current maximum and minimum subarray sums,
-  // overall sum, maximum sum, and minimum sum
-  let curMax = 0,
-    curMin = 0,
-    sum = 0,
-    maxSum = nums[0],
-    minSum = nums[0];
-
-  // Iterate through the array
-  for (let num of nums) {
-    // Calculate the current maximum subarray sum using Kadane's algorithm
-    curMax = Math.max(curMax, 0) + num;
-    // Update the maximum sum if the current maximum is greater
-    maxSum = Math.max(maxSum, curMax);
-
-    // Calculate the current minimum subarray sum using Kadane's algorithm
-    curMin = Math.min(curMin, 0) + num;
-    // Update the minimum sum if the current minimum is smaller
-    minSum = Math.min(minSum, curMin);
-
-    // Calculate the overall sum of the array
-    sum += num;
-  }
-
-  // If the overall sum is equal to the minimum sum, it means all elements are negative,
-  // so return the maximum sum (the maximum subarray sum in this case)
-  if (sum === minSum) {
-    return maxSum;
-  }
-
-  // Otherwise, return the maximum of the maximum sum and the difference between
-  // the overall sum and the minimum sum (considering the circular nature of the array)
-  return Math.max(maxSum, sum - minSum);
 };
 
 /* String compression - Given an array of characters chars, compress it using the following algorithm:
@@ -393,5 +221,158 @@ const compress = (chars) => {
   }
 
   return newLen;
+};
+
+/* Maximum Subarray - Given an integer array nums, find the subarray with the largest sum, and return its sum.
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = (nums) => {
+
+  /* pseudo code
+    move through nums 
+      keep tracking the sum of curr sub array and max found so far
+      ditch the subarray when the sum becomes negative
+  */
+
+  let max = -Infinity;
+  let currSubSum = 0;
+
+  for (const num of nums) {
+    currSubSum += num;
+    max = Math.max(max, currSubSum);
+    currSubSum = Math.max(currSubSum, 0);
+  }
+
+  return max;
+};
+
+/* Longest consecutive sequence - Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestConsecutive = (nums) => {
+
+  /* pseudo code
+    move through nums
+      check if curr num is the end of a sequence
+        loop through the set with decreasing num
+          track the count
+      track the max length of increasing seq
+  */
+
+  const set = new Set(nums);
+
+  let max = 0;
+
+  for (const num of nums) {
+    if (set.has(num + 1)) {
+      continue;
+    }
+
+    let counter = 1;
+    let curr = num;
+
+    while (set.has(--curr)) {
+      counter++;
+    }
+
+    max = Math.max(max, counter);
+  }
+
+  return max;
+};
+
+/* Maximum Product Subarray - Given an integer array nums, find a subarray that has the largest product, and return the product
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxProductS = (nums) => {
+
+  /* pseudo code
+    track max and min products found so far
+    move i through the nums array
+      check if curr num increase max or min product or we should discard the prev subarray
+      keep tracking the max
+  */
+
+  if (!nums.length) {
+    return 0;
+  }
+
+  let maxProductSoFar = nums[0];
+  let minProductSoFar = nums[0];
+
+  let max = maxProductSoFar;
+
+  for (let i = 1; i < nums.length; i++) {
+    const curr = nums[i];
+
+    const tempMax = Math.max(
+      curr,
+      Math.max(curr * maxProductSoFar, curr * minProductSoFar)
+    );
+
+    minProductSoFar = Math.min(
+      curr,
+      Math.min(curr * maxProductSoFar, curr * minProductSoFar)
+    );
+
+    maxProductSoFar = tempMax;
+
+    max = Math.max(max, maxProductSoFar);
+  }
+
+  return max;
+};
+
+/* Max sum circular subarray - Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+
+A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubarraySumCircular = (nums) => {
+
+  /* pseudo code
+    move through nums
+      track curr subarray max sum, max sum, curr subarray min sum, min sum so far
+  */
+
+  let curMax = 0,
+    curMin = 0,
+    sum = 0,
+    maxSum = nums[0],
+    minSum = nums[0];
+
+  for (const num of nums) {
+    curMax = Math.max(curMax, 0) + num;
+    maxSum = Math.max(maxSum, curMax);
+
+    curMin = Math.min(curMin, 0) + num;
+    minSum = Math.min(minSum, curMin);
+
+    sum += num;
+  }
+
+  if (sum === minSum) {
+    return maxSum;
+  }
+
+  return Math.max(maxSum, sum - minSum);
 };
 
