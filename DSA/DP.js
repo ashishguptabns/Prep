ef8504985d3ab8b9a2bb8ea2615d9d6b8d327610
ashishguptabns <ashishguptabns@gmail.com>
2024-03-31 17:29:46 +0530

    Update DP.js

--- DSA/DP.js
+++ DSA/DP.js
@@ -1369,3 +1369,36 @@ var minCostColor = function (colors, neededTime) {
 };
 
 /* 678. Valid Parenthesis String */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function (s) {
+    let openStack = [];
+    let asteriskStack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            openStack.push(i);
+        } else if (s[i] === '*') {
+            asteriskStack.push(i);
+        } else {
+            if (openStack.length > 0) {
+                openStack.pop();
+            } else if (asteriskStack.length > 0) {
+                asteriskStack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    while (openStack.length > 0 && asteriskStack.length > 0) {
+        if (openStack.pop() > asteriskStack.pop()) {
+            return false;
+        }
+    }
+
+    return openStack.length === 0;
+};
