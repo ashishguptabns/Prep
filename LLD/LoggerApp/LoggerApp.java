c06a2176300c4f0351442c5bc29e4274dac10a1d
ashishguptabns <ashishguptabns@gmail.com>
2025-09-25 18:59:08 +0530

    LoggerApp

--- LLD/LoggerApp/LoggerApp.java
+++ LLD/LoggerApp/LoggerApp.java
@@ -1,13 +1,25 @@
-package LLD;
+package LLD.LoggerApp;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 class LogMessage {
     private final String msg;
+    private final long ts;
 
     public LogMessage(String msg) {
         this.msg = msg;
+        this.ts = System.currentTimeMillis();
+    }
+
+    public String format() {
+        return this.ts + " - " + msg;
     }
 }
 
@@ -15,9 +27,38 @@ interface ILogger {
     public void log(LogMessage msg);
 }
 
+class LogWriter {
+    private BufferedWriter writer = null;
+
+    public LogWriter(String fileName) {
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName, true);
+            writer = new BufferedWriter(new OutputStreamWriter(fos));
+        } catch (FileNotFoundException e) {
+        }
+    }
+
+    public synchronized void write(LogMessage msg) {
+        try {
+            writer.write(msg.format());
+            writer.newLine();
+            writer.flush();
+        } catch (IOException ex) {
+        }
+    }
+
+    public void close() {
+        try {
+            writer.close();
+        } catch (IOException e) {
+        }
+    }
+}
+
 class LogWorker extends Thread {
     private final BlockingDeque<LogMessage> queue;
     private volatile boolean running = true;
+    private final LogWriter writer = new LogWriter("log_file.log");
 
     public LogWorker(BlockingDeque<LogMessage> queue) {
         this.queue = queue;
@@ -26,8 +67,20 @@ class LogWorker extends Thread {
     @Override
     public void run() {
         while (this.running || !queue.isEmpty()) {
-            
+            try {
+                LogMessage msg = queue.poll(1, TimeUnit.SECONDS);
+                if (msg != null) {
+                    writer.write(msg);
+                }
+            } catch (Exception e) {
+            }
         }
+        writer.close();
+    }
+
+    public void shutDown() {
+        this.running = false;
+        this.interrupt();
     }
 }
 
