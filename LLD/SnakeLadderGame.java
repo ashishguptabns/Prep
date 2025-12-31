8991b89262ad2eef12e461d5d06117ffe5eb8530
ashishguptabns <ashishguptabns@gmail.com>
2025-12-31 20:32:15 +0530

    Update SnakeLadderGame.java

--- LLD/SnakeLadderGame.java
+++ LLD/SnakeLadderGame.java
@@ -5,8 +5,9 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
 public class SnakeLadderGame {
     public static void main(String[] args) {
         Board board = new Board(100);
-        board.setJump(14, 4); // Snake
-        board.setJump(7, 31); // Ladder
+        board.setCell(14, new SnakeCell(4));
+        board.setCell(7, new LadderCell(31));
+
         try {
             runGame(board, 10);
         } catch (InterruptedException e) {
@@ -39,20 +40,63 @@ public class SnakeLadderGame {
     }
 }
 
+interface Cell {
+    int resolve(int current);
+}
+
+class NormalCell implements Cell {
+    final int endPos;
+
+    public NormalCell(int endPos) {
+        this.endPos = endPos;
+    }
+
+    @Override
+    public int resolve(int current) {
+        return endPos;
+    }
+}
+
+class SnakeCell implements Cell {
+    final int endPos;
+
+    public SnakeCell(int destination) {
+        this.endPos = destination;
+    }
+
+    @Override
+    public int resolve(int current) {
+        return endPos;
+    }
+}
+
+class LadderCell implements Cell {
+    final int endPos;
+
+    public LadderCell(int destination) {
+        this.endPos = destination;
+    }
+
+    @Override
+    public int resolve(int current) {
+        return endPos;
+    }
+}
+
 class Board {
-    private final int[] cells;
+    private final Cell[] cells;
     private final AtomicIntegerArray playerPositions;
 
     public Board(int size) {
-        this.cells = new int[size + 1];
+        this.cells = new Cell[size + 1];
         for (int i = 0; i <= size; i++) {
-            cells[i] = i;
+            cells[i] = new NormalCell(i);
         } // Default: No jump
         this.playerPositions = new AtomicIntegerArray(1000); // Support 1000 players
     }
 
-    public void setJump(int start, int end) {
-        cells[start] = end;
+    public void setCell(int i, Cell cell) {
+        cells[i] = cell;
     }
 
     public int movePlayer(int playerId, int roll) {
@@ -60,7 +104,7 @@ class Board {
         do {
             current = playerPositions.get(playerId);
             next = Math.min(current + roll, cells.length - 1);
-            next = cells[next]; // Apply snake/ladder logic
+            next = cells[next].resolve(current); // Apply snake/ladder logic
         } while (!playerPositions.compareAndSet(playerId, current, next));
         return next;
     }
