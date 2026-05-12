d16f3e73be561a8f102b80ae90867437ac4dcfc2
ashishguptabns <ashishguptabns@gmail.com>
2026-05-12 18:00:15 +0530

    Create RequestLoggingFilter.java

---
+++ RackApp/src/main/java/com/example/rackapp/filter/RequestLoggingFilter.java
@@ -0,0 +1,32 @@
+package com.example.rackapp.filter;
+
+import java.io.IOException;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (request instanceof HttpServletRequest httpRequest) {
+            String method = httpRequest.getMethod();
+            String uri = httpRequest.getRequestURI();
+            String remoteAddr = request.getRemoteAddr();
+            System.out.println("Servlet Filter: " + method + " " + uri + " from " + remoteAddr);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
