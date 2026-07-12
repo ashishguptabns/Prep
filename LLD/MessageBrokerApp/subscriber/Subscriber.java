be17ba5376721404a311931d37051652b3096009
ashishguptabns <ashishguptabns@gmail.com>
2026-07-12 17:21:28 +0530

    NA

--- LLD/MessageBrokerApp/subscriber/Subscriber.java
+++
@@ -1,8 +0,0 @@
-package LLD.MessageBrokerApp.subscriber;
-
-import LLD.MessageBrokerApp.model.Message;
-
-public interface Subscriber {
-    void onMessage(Message message);
-    String getSubscriberId();
-}
